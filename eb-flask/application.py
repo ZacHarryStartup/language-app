3e79550be4422f142bfbd819e02f212069f0129a
Zac Simunovic <z5425797@ad.unsw.edu.au>
2024-06-16 13:46:08 +1000

    updates to config files

---
+++ eb-flask/application.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/value', methods=['GET'])
+def get_value():
+    return jsonify({'value': '42'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
