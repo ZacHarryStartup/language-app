5e9843ac3e0f7935268e11782b159ebdcf11f151
Zac Simunovic <z5425797@ad.unsw.edu.au>
2024-06-16 14:03:24 +1000

    change to 5000

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -8,5 +8,5 @@ def hello():
     return "Hello from Flask!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))  # Default to 8080
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000
     app.run(host='0.0.0.0', port=port)
