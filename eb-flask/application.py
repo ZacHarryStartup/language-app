50b2a11be00f5ff78e633d70df091a7b4e28dcff
Zac Simunovic <z5425797@ad.unsw.edu.au>
2024-06-16 14:08:07 +1000

    chnage start up

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,12 +1,11 @@
-import os
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello():
     return "Hello from Flask!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000
-    app.run(host='0.0.0.0', port=port)
+    application.debug = True
+    application.run()
