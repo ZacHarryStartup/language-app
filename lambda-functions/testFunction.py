2fd8c1f5f287cc5d77578c9fe305b0973bf1d7df
Harry Kim <harrison.falconer2@gmail.com>
2024-06-18 05:47:30 +0000

    Automatic commit from createLambda workflow

---
+++ lambda-functions/testFunction.py
@@ -0,0 +1,14 @@
+import boto3
+import json
+
+# define the DynamoDB table that Lambda will connect to
+tableName = "lambda-apigateway"
+
+# create the DynamoDB resource
+dynamo = boto3.resource('dynamodb').Table(tableName)
+
+print('Loading function')
+
+def lambda_handler(event, context):
+    return
+    
