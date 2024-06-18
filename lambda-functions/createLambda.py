3ce6ca4161e347b9d39435bb16420e048a49e258
Harry Kim <harrison.falconer2@gmail.com>
2024-06-18 15:00:31 +1000

    Testing complete

--- lambda-functions/createLambda.py
+++ lambda-functions/createLambda.py
@@ -2,8 +2,8 @@ import argparse
 import os
 
 def main(functionName):
-    f = open('lambda-functions/' + functionName + '.py', "w")
-    fileContent = """import boto3
+    pythonFile = open('lambda-functions/' + functionName + '.py', "w")
+    pythonContent = """import boto3
 import json
 
 # define the DynamoDB table that Lambda will connect to
@@ -17,9 +17,46 @@ print('Loading function')
 def lambda_handler(event, context):
     return
     """
-    f.write(fileContent)
-    test = open('lambda-functions/' + functionName + '.py', "r")
-    print(test.read())
+    pythonFile.write(pythonContent)
+
+    ymlFile = open('.github/workflows/' + functionName + '.yml', "w")
+
+    ymlContent = """name: Lambda update workflow for {lambdaFunction}
+on:
+  workflow_dispatch:
+  push:
+    branches:
+      - main
+    paths:
+      -  lambda-functions/{lambdaFunction}.py
+env:
+  functionName: {lambdaFunction}
+  AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
+  AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  AWS_REGION: ap-southeast-2
+jobs:
+  updateLambda:
+    runs-on: ubuntu-latest
+    environment: language-app
+    steps:
+      - name: checkout_repo
+        uses: actions/checkout@v3
+      - name: test secrets
+        run: |
+          echo ${{ secrets.AWS_ACCESS_KEY_ID }}
+      - name: configure aws credentials
+        uses: aws-actions/configure-aws-credentials@v4
+        with:
+          aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+          aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+          aws-region: ap-southeast-2
+      - name: "Zip function"
+        run: |
+          zip code.zip lambda-functions/${{ env.functionName }}.py 
+      - name: "Updated lambda"
+        run: aws lambda update-function-code --function-name ${{ env.functionName }} --zip-file fileb://code.zip
+""".format(lambdaFunction=functionName)
+
 
 def parseArgs():
     parser = argparse.ArgumentParser()
