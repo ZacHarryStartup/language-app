cf621662c60226cce5475b1d1586cd13ad49d667
Harry Kim <harrison.falconer2@gmail.com>
2024-06-18 14:19:37 +1000

    Added automation for setting up lambda function workflow

---
+++ lambda-functions/createLambda.py
@@ -0,0 +1,34 @@
+import argparse
+
+
+def main(functionName):
+    f = open(functionName + '.py', "w")
+    fileContent = """import boto3
+import json
+
+# define the DynamoDB table that Lambda will connect to
+tableName = "lambda-apigateway"
+
+# create the DynamoDB resource
+dynamo = boto3.resource('dynamodb').Table(tableName)
+
+print('Loading function')
+
+def lambda_handler(event, context):
+return
+    """
+    f.write(fileContent)
+
+    
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f",
+        "--functionName",
+        required=True
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main(parseArgs())
