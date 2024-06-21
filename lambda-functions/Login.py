5d586e4b693a6a9143fa50c258492608a09f32c1
Zac Simunovic <z5425797@ad.unsw.edu.au>
2024-06-21 19:22:28 +1000

    updated auth functions with cognito details

--- lambda-functions/Login.py
+++ lambda-functions/Login.py
@@ -3,9 +3,9 @@ import boto3
 from botocore.exceptions import BotoCoreError, ClientError
 
 # Initialize Cognito client
-cognito_client = boto3.client('cognito-idp', region_name='your-region')  # Replace 'your-region' with your AWS region
-user_pool_id = 'your-user-pool-id'  # Replace with your Cognito User Pool ID
-client_id = 'your-client-id'  # Replace with your Cognito App Client ID
+cognito_client = boto3.client('cognito-idp', region_name='ap-southeast-2')  # Replace 'your-region' with your AWS region
+user_pool_id = 'ap-southeast-2_CGcEJ2Fcb'  # Replace with your Cognito User Pool ID
+client_id = '5uer2o6e6atje9f29se6q4t029'  # Replace with your Cognito App Client ID
 
 #@app.route('/login', methods=['POST'])
 def login():
