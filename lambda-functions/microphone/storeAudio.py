e226b4eb1e2e5e0f257b3036ae65c9da59b4d77a
Harry Kim <harrison.falconer2@gmail.com>
2024-06-22 14:48:02 +1000

    Added openai stt

--- lambda-functions/microphone/storeAudio.py
+++ lambda-functions/microphone/storeAudio.py
@@ -12,18 +12,22 @@ tableName = "lambda-apigateway"
 # create the DynamoDB resource
 dynamo = boto3.resource('dynamodb').Table(tableName)
 
-print('Loading function')
+print('Running audio function')
 
 def lambda_handler(event, context):
-    audio_file= open("audio.m4a", "rb")
-    transcription = client.audio.transcriptions.create(
-    model="whisper-1", 
-    file=audio_file,
-    )
+    # audio_file= open("audio.m4a", "rb")
+    # transcription = client.audio.transcriptions.create(
+    # model="whisper-1", 
+    # file=audio_file,
+    # )
+    print(event, context)
+    # message = {
+    #     'message': transcription.text
+    # }
     message = {
-        'message': transcription.text
+        'message': "hi"
     }
-    print(transcription.text)
+    # print(transcription.text)
     return {
         'statusCode': 200,
         'headers': {'Content-Type': 'application/json'},
