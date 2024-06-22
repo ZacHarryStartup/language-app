506948e25094f24c2efb35f655cb6062e466a1cd
Zac Simunovic <z5425797@ad.unsw.edu.au>
2024-06-23 09:34:40 +1000

    update register handler

--- lambda-functions/Auth/Register.py
+++ lambda-functions/Auth/Register.py
@@ -7,7 +7,7 @@ cognito_client = boto3.client('cognito-idp', region_name='ap-southeast-2')
 user_pool_id = 'ap-southeast-2_CGcEJ2Fcb'
 client_id = '5uer2o6e6atje9f29se6q4t029'
 
-def register(event, context):
+def lambda_handler(event, context):
     try:
         # Directly parse event assuming it's a JSON payload
         data = json.loads(event.get('body', '{}'))
