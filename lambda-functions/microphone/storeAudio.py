8dcf57c7f7d7836efb4e0e08643765c9adc492b0
Harry Kim <harrison.falconer2@gmail.com>
2024-06-22 15:00:22 +1000

    Added openai stt

--- lambda-functions/microphone/storeAudio.py
+++ lambda-functions/microphone/storeAudio.py
@@ -1,11 +1,7 @@
 import boto3
 import json
 from openai import OpenAI
-import logging
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-client = OpenAI(api_key="sk-proj-FeXYZfbbwupEo7KpLtNIT3BlbkFJGIfLI7JZqGGK8abz2WoP")
+client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
 
 
@@ -16,31 +12,20 @@ tableName = "lambda-apigateway"
 # create the DynamoDB resource
 dynamo = boto3.resource('dynamodb').Table(tableName)
 
-print('Running audio function')
+print('Loading function')
 
 def lambda_handler(event, context):
-    try:
-        audio_file= open("audio.m4a", "rb")
-        transcription = client.audio.transcriptions.create(
-        model="whisper-1", 
-        file=audio_file,
-        )
-        print(event, context)
-        message = {
-            'message': transcription.text
-        }
-        message = {
-            'message': "hi"
-        }
-        # print(transcription.text)
-        return {
-            'statusCode': 200,
-            'headers': {'Content-Type': 'application/json'},
-            'body': json.dumps(message)
-        }
-    except ClientError as e:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': str(e)})
-        }
+    audio_file= open("audio.m4a", "rb")
+    transcription = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file,
+    )
+    message = {
+        'message': transcription.text
+    }
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(message)
+    }
     
