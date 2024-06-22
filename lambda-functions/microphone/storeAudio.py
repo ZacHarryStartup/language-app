4d6d3ac69cc87fed61b7989a17fa53d8ebc5818e
Harry Kim <harrison.falconer2@gmail.com>
2024-06-22 15:05:26 +1000

    Added openai stt

--- lambda-functions/microphone/storeAudio.py
+++ lambda-functions/microphone/storeAudio.py
@@ -1,11 +1,17 @@
 import boto3
 import json
-# from openai import OpenAI
-# client = OpenAI(api_key="sk-proj-FeXYZfbbwupEo7KpLtNIT3BlbkFJGIfLI7JZqGGK8abz2WoP")
+
+# define the DynamoDB table that Lambda will connect to
+tableName = "lambda-apigateway"
+
+# create the DynamoDB resource
+dynamo = boto3.resource('dynamodb').Table(tableName)
 
 print('Loading function')
 
 def lambda_handler(event, context):
-    
-    return "Lets pretend this is stored"
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Hi")
+    }
     
