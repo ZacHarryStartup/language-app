491bc899c7c38ad7d0cfc9a94200681297512375
Zac Simunovic <z5425797@ad.unsw.edu.au>
2024-06-23 09:48:25 +1000

    initiating update to lambda functions, after harry fixed

--- lambda-functions/Auth/Resend_Confirmation_Code.py
+++ lambda-functions/Auth/Resend_Confirmation_Code.py
@@ -2,13 +2,13 @@ from flask import Flask, request, jsonify
 import boto3
 from botocore.exceptions import BotoCoreError, ClientError
 
-# Initialize Cognito client
+# Initialize Cognito client-
 cognito_client = boto3.client('cognito-idp', region_name='ap-southeast-2')  # Replace 'your-region' with your AWS region
 user_pool_id = 'ap-southeast-2_CGcEJ2Fcb'  # Replace with your Cognito User Pool ID
 client_id = '5uer2o6e6atje9f29se6q4t029'  # Replace with your Cognito App Client ID
 
 #@app.route('/resend-confirmation-code', methods=['POST'])
-def resend_confirmation_code():
+def lambda_handler():
     try:
         data = request.json
         response = cognito_client.resend_confirmation_code(
